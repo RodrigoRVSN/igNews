dc0cca84e90cc78644c2075f5bed6840d0e3beef
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-10-12 15:34:24 -0300

    💸 stripe + fauna integration

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,9 +1,39 @@
 import styles from "./styles.module.scss";
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  return <button className={styles.subscribeButton}>Subscribe now</button>;
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+    try {
+      const response = await api.post<any>("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      console.log("stripe");
+      console.log(stripe);
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <button
+      className={styles.subscribeButton}
+      onClick={() => handleSubscribe()}
+    >
+      Subscribe now
+    </button>
+  );
 }
